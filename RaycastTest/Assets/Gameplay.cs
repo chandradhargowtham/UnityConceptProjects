6a0d37a40b19087296d8c1b747169d29c5f0c9f2
Chandradhar Gowtham Cherukuvada <chandradhargowtham93@gmail.com>
2020-04-09 02:11:17 +0530

    Added Basic Raycast Example Project

---
+++ RaycastTest/Assets/Gameplay.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Vector3 destinationVector = new Vector3();
+        
+        if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0) && hit.transform.gameObject != GameObject.Find("Cube"))
+        {
+            
+            
+                destinationVector = hit.transform.position;
+            
+            GameObject.Find("Cube").transform.position = ray.GetPoint(10);
+            
+            
+
+        }
+        //
+    }
+}
