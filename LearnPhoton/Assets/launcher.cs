c3b03c9ce0859eb0306dd7023d63ce00e3d801c6
ChandradharCherukuvada <chandradhar93@pm.me>
2020-10-08 14:08:29 +0530

    Photon 2 Basic Project

---
+++ LearnPhoton/Assets/launcher.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class launcher : MonoBehaviourPunCallbacks
+{
+    public GameObject currentObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.LogError("Connected To Master");
+        PhotonNetwork.JoinOrCreateRoom("room",new RoomOptions { MaxPlayers=3},TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.LogError("Room Joined");
+        currentObject= PhotonNetwork.Instantiate("Cube", new Vector3(0, 0, 0), Quaternion.identity);
+
+    }
+
+    private void Update()
+    {
+        if(currentObject!=null)
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            currentObject.transform.Translate(Vector3.up);
+        }
+    }
+
+
+}
